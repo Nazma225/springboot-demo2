e31069799da95445ac3e5c97bc830c5ae5d2773c
Nazma Nazma <nazma.java@gmail.com>
2022-05-26 10:32:13 -0500

    updated .java file

--- src/main/java/com/training/basic/demo/controller/Demo2Controller.java
+++ src/main/java/com/training/basic/demo/controller/Demo2Controller.java
@@ -13,15 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo/v2")
 public class Demo2Controller {
 
-	@GetMapping(value="/{name}")
+	@GetMapping(value="/name/{name}")
 	public String fetchName(@PathVariable(name="name") String name) {
 		return "Hello "+name;
+	}
+	
+	@GetMapping(value="/address/{address}")
+	public String getAddress(@PathVariable(name="address") String address) {
+		return "Address is:"+address;
+		
+	}
+	
+	@GetMapping(value="/technology/{technology}")
+	public String getTechnology(@PathVariable(name="technology") String technology) {
+		return "Technology is:"+ technology;
+		
 	}
 	
 	@PostMapping(value="/add")
 	public String addName(@RequestBody Map<String, String> map) {
 		String name = map.get("name");
 		String address = map.get("address");
-		return "Hello "+name + ", Your Address is: "+address  ;
+		String tech=map.get("technology");
+		String phno=map.get("phno");
+		return "Hello "+name + ", Your Address is: "+address+", Technology is:"+tech+" and your phone number is:"+phno  ;
 	}
 }
